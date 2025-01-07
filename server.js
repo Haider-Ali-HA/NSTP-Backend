b0d8cf048cebe902312885ac0c8623e3639632d6
Haider Ali <haideraliatish500@gmail.com>
2025-01-07 23:41:45 +0500

    change the cors policy

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ mongoose
 
 app.use(
   cors({
-    origin: 'https://nstpweb.vercel.app', // Your frontend URL
+    origin: 'https://nstpweb.vercel.app/', // Your frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
